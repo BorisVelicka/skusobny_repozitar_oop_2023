fc363250cb5784dc2b43443e41eb6447aa04e7f1
BorisVelicka <xvelickab@stuba.sk>
2023-02-19 15:40:37 +0100

    uz na to nemam nervy

--- skuska_maven/src/main/java/sk/fei/oop/App.java
+++ skuska_maven/src/main/java/sk/fei/oop/App.java
@@ -2,6 +2,6 @@ package sk.fei.oop;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("skuska JaVA");
+        System.out.println("skuska JAVA");
     }
 }
